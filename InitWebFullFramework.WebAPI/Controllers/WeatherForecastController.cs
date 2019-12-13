afb90d6cd6cc2fd11c5dc3e88332e99cd837232d
Denis Mušić <37282231+denis-music@users.noreply.github.com>
2019-12-13 15:37:18 +0100

    Update WeatherForecastController.cs

--- InitWebFullFramework.WebAPI/Controllers/WeatherForecastController.cs
+++ InitWebFullFramework.WebAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace InitWebFullFramework.WebAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Mahe Car, ""Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
